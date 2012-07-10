070e4e086acdf1d1856574a1ba8dd2fee9e2e739
Nathan D. Holmes <maverick@drgw.net>
2012-07-10 04:43:02 +0000

    Initial cut of mrbw-th

---
+++ src/mrbw-th.c
@@ -0,0 +1,405 @@
+/*************************************************************************
+Title:    MRBus AVR Template
+Authors:  Nathan D. Holmes <maverick@drgw.net>
+File:     $Id: $
+License:  GNU General Public License v3
+
+LICENSE:
+    Copyright (C) 2012 Nathan Holmes
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+*************************************************************************/
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <util/delay.h>
+
+#ifdef MRBEE
+// If wireless, redefine the common variables and functions
+#include "mrbee.h"
+#define mrbus_rx_buffer mrbee_rx_buffer
+#define mrbus_tx_buffer mrbee_tx_buffer
+#define mrbus_state mrbee_state
+#define MRBUS_TX_PKT_READY MRBEE_TX_PKT_READY
+#define MRBUS_RX_PKT_READY MRBEE_RX_PKT_READY
+#define mrbux_rx_buffer mrbee_rx_buffer
+#define mrbus_tx_buffer mrbee_tx_buffer
+#define mrbus_state mrbee_state
+#define mrbusInit mrbeeInit
+#define mrbusPacketTransmit mrbeePacketTransmit
+#endif
+
+#include "mrbus.h"
+
+extern uint8_t mrbus_activity;
+extern uint8_t mrbus_rx_buffer[MRBUS_BUFFER_SIZE];
+extern uint8_t mrbus_tx_buffer[MRBUS_BUFFER_SIZE];
+extern uint8_t mrbus_state;
+
+uint8_t mrbus_dev_addr = 0;
+
+
+
+#ifdef ACCURATE_TIMER
+
+// ******** Start 100 Hz Timer - Very Accurate Version
+
+// Initialize a 100Hz timer for use in triggering events.
+// If you need the timer resources back, you can remove this, but I find it
+// rather handy in triggering things like periodic status transmissions.
+// If you do remove it, be sure to yank the interrupt handler and ticks/secs as well
+// and the call to this function in the main function
+
+uint8_t ticks;
+uint8_t secs;
+
+void initialize100HzTimer(void)
+{
+	// Set up timer 1 for 100Hz interrupts
+	TCCR1A = 0;
+	TCCR1B = _BV(CS11) | _BV(CS10);
+	TCCR1C = 0;
+	TIMSK1 = _BV(TOIE1);
+	ticks = 0;
+	secs = 0;
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	TCNT1 += 0xF3CB;
+	ticks++;
+	if (ticks >= 100)
+	{
+		ticks = 0;
+		secs++;
+	}
+}
+
+#else
+
+// ******** Start 100 Hz Timer, 0.16% error version (Timer 0)
+// If you can live with a slightly less accurate timer, this one only uses Timer 0, leaving Timer 1 open
+// for more advanced things that actually need a 16 bit timer/counter
+
+// Initialize a 100Hz timer for use in triggering events.
+// If you need the timer resources back, you can remove this, but I find it
+// rather handy in triggering things like periodic status transmissions.
+// If you do remove it, be sure to yank the interrupt handler and ticks/secs as well
+// and the call to this function in the main function
+
+uint8_t ticks;
+uint8_t secs;
+
+void initialize100HzTimer(void)
+{
+	// Set up timer 1 for 100Hz interrupts
+	TCNT0 = 0;
+	OCR0A = 0xC2;
+	ticks = 0;
+	secs = 0;
+	TCCR0A = _BV(WGM01);
+	TCCR0B = _BV(CS02) | _BV(CS00);
+	TIMSK0 |= _BV(OCIE0A);
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+	if (++ticks >= 100)
+	{
+		ticks = 0;
+		secs++;
+	}
+	PORTB ^= _BV(PB0);
+}
+
+// End of 100Hz timer
+
+#endif
+
+void PktHandler(void)
+{
+	uint16_t crc = 0;
+	uint8_t i;
+
+	//*************** PACKET FILTER ***************
+	// Loopback Test - did we send it?  If so, we probably want to ignore it
+	if (mrbus_rx_buffer[MRBUS_PKT_SRC] == mrbus_dev_addr) 
+		goto	PktIgnore;
+
+	// Destination Test - is this for us or broadcast?  If not, ignore
+	if (0xFF != mrbus_rx_buffer[MRBUS_PKT_DEST] && mrbus_dev_addr != mrbus_rx_buffer[MRBUS_PKT_DEST]) 
+		goto	PktIgnore;
+	
+	// CRC16 Test - is the packet intact?
+	for(i=0; i<mrbus_rx_buffer[MRBUS_PKT_LEN]; i++)
+	{
+		if ((i != MRBUS_PKT_CRC_H) && (i != MRBUS_PKT_CRC_L)) 
+			crc = mrbusCRC16Update(crc, mrbus_rx_buffer[i]);
+	}
+	if ((UINT16_HIGH_BYTE(crc) != mrbus_rx_buffer[MRBUS_PKT_CRC_H]) || (UINT16_LOW_BYTE(crc) != mrbus_rx_buffer[MRBUS_PKT_CRC_L]))
+		goto	PktIgnore;
+		
+	//*************** END PACKET FILTER ***************
+
+
+	//*************** PACKET HANDLER - PROCESS HERE ***************
+
+	// Just smash the transmit buffer if we happen to see a packet directed to us
+	// that requires an immediate response
+	//
+	// If we're in here, then either we're transmitting, then we can't be 
+	// receiving from someone else, or we failed to transmit whatever we were sending
+	// and we're waiting to try again.  Either way, we're not going to corrupt an
+	// in-progress transmission.
+	//
+	// All other non-immediate transmissions (such as scheduled status updates)
+	// should be sent out of the main loop so that they don't step on things in
+	// the transmit buffer
+	
+	if ('A' == mrbus_rx_buffer[MRBUS_PKT_TYPE])
+	{
+		// PING packet
+		mrbus_tx_buffer[MRBUS_PKT_DEST] = mrbus_rx_buffer[MRBUS_PKT_SRC];
+		mrbus_tx_buffer[MRBUS_PKT_SRC] = mrbus_dev_addr;
+		mrbus_tx_buffer[MRBUS_PKT_LEN] = 6;
+		mrbus_tx_buffer[MRBUS_PKT_TYPE] = 'a';
+		mrbus_state |= MRBUS_TX_PKT_READY;
+		goto PktIgnore;
+	} 
+	else if ('W' == mrbus_rx_buffer[MRBUS_PKT_TYPE]) 
+	{
+		// EEPROM WRITE Packet
+		mrbus_tx_buffer[MRBUS_PKT_DEST] = mrbus_rx_buffer[MRBUS_PKT_SRC];
+		mrbus_tx_buffer[MRBUS_PKT_LEN] = 8;			
+		mrbus_tx_buffer[MRBUS_PKT_TYPE] = 'w';
+		eeprom_write_byte((uint8_t*)(uint16_t)mrbus_rx_buffer[6], mrbus_rx_buffer[7]);
+		mrbus_tx_buffer[6] = mrbus_rx_buffer[6];
+		mrbus_tx_buffer[7] = mrbus_rx_buffer[7];
+		if (MRBUS_EE_DEVICE_ADDR == mrbus_rx_buffer[6])
+			mrbus_dev_addr = eeprom_read_byte((uint8_t*)MRBUS_EE_DEVICE_ADDR);
+		mrbus_tx_buffer[MRBUS_PKT_SRC] = mrbus_dev_addr;
+		mrbus_state |= MRBUS_TX_PKT_READY;
+		goto PktIgnore;
+	
+	}
+	else if ('R' == mrbus_rx_buffer[MRBUS_PKT_TYPE]) 
+	{
+		// EEPROM READ Packet
+		mrbus_tx_buffer[MRBUS_PKT_DEST] = mrbus_rx_buffer[MRBUS_PKT_SRC];
+		mrbus_tx_buffer[MRBUS_PKT_LEN] = 8;			
+		mrbus_tx_buffer[MRBUS_PKT_TYPE] = 'r';
+		mrbus_tx_buffer[6] = mrbus_rx_buffer[6];
+		mrbus_tx_buffer[7] = eeprom_read_byte((uint8_t*)(uint16_t)mrbus_rx_buffer[6]);			
+		mrbus_state |= MRBUS_TX_PKT_READY;
+		goto PktIgnore;
+	}
+
+	// FIXME:  Insert code here to handle incoming packets specific
+	// to the device.
+
+	//*************** END PACKET HANDLER  ***************
+
+	
+	//*************** RECEIVE CLEANUP ***************
+PktIgnore:
+	// Yes, I hate gotos as well, but sometimes they're a really handy and efficient
+	// way to jump to a common block of cleanup code at the end of a function 
+
+	// This section resets anything that needs to be reset in order to allow us to receive
+	// another packet.  Typically, that's just clearing the MRBUS_RX_PKT_READY flag to 
+	// indicate to the core library that the mrbus_rx_buffer is clear.
+	mrbus_state &= (~MRBUS_RX_PKT_READY);
+	return;	
+}
+
+void init(void)
+{
+	// FIXME:  Do any initialization you need to do here.
+	DDRC &= ~_BV(PC3);
+	PORTC &= ~_BV(PC3);
+
+	// Initialize MRBus address from EEPROM address 1
+	mrbus_dev_addr = eeprom_read_byte((uint8_t*)MRBUS_EE_DEVICE_ADDR);
+}
+
+
+uint8_t dht11_bitnum=0;
+uint8_t dht11_data[5];
+uint8_t dht11_read_complete=0;
+
+void initializeDHT11Timer()
+{
+	// Set up timer 1 for 100Hz interrupts
+	TCNT2 = 0;
+	OCR2A = 0xC2;
+	TCCR2A = 0;
+	TCCR2B = _BV(CS21);
+}
+
+ISR(PCINT1_vect)
+{
+	// This is used by the DHT11 code to read asynchronously to the main loop
+	if(PINC & _BV(PC3))
+	{
+		uint8_t timerval = TCNT2;
+		// It was the rising edge.
+		// 26-28ms in the timer indicates that it's a 0, 70ms indicates a 1
+		if (timerval > X && timerval < Y)
+		{
+			// It's a one, put one in the correct place
+			dht11_data[dht11_bitnum / 8] |= 1<<(7-(dht11_bitnum % 8));
+		
+		}
+		dht11_bitnum++;
+		TCNT2 = 0;	
+	}
+	else
+	{
+		// It's the falling edge, indicating that we're at the beginning of a clock period
+		// Reset the timer
+		TCNT2 = 0;
+		TIMSK2 |= _BV(TOIE2);
+	}
+
+	if(dht11_bitnum > 40)
+	{
+		// We're done, indicate complete and shut down the interrupt
+		dht_read_complete = 1;
+
+		// Shut down the PCINT11 interrupt
+		PCIFR |= _BV(PCIF1);
+		PCICR &= ~_BV(PCIE1);
+		
+		TIMSK2 &= ~_BV(TOIE2);
+	
+	}
+}
+
+
+ISR(TIMER2_OVF_vect)
+{
+	// We overflowed, meaning the DHT11 stopped responding correctly
+	dht_read_complete = 2;
+
+	// Shut down Timer 2
+	TIMSK2 &= ~_BV(TOIE2);
+
+	// Shut down the PCINT11 interrupt
+	PCIFR |= _BV(PCIF1);
+	PCICR &= ~_BV(PCIE1);
+}
+
+
+
+int main(void)
+{
+	// Application initialization
+	init();
+
+	// Initialize a 100 Hz timer.  See the definition for this function - you can
+	// remove it if you don't use it.
+	initialize100HzTimer();
+
+	// Initialize MRBus core
+	mrbusInit();
+
+	sei();	
+
+	while (1)
+	{
+#ifdef MRBEE
+		mrbeePoll();
+#endif
+		// Handle any packets that may have come in
+		if (mrbus_state & MRBUS_RX_PKT_READY)
+			PktHandler();
+			
+		// FIXME: Do any module-specific behaviours here in the loop.
+		if (secs >= 2)
+		{
+			uint8_t countdown = 20;
+			// Get triggery
+			// Set DHT11 data wire to ground for 20ms
+
+			dht11_bitnum=0;
+			memset(dht11_data, 0, sizeof(dht11_data));
+			dht11_read_complete=0;
+			
+			DDRC |= _BV(PC3);	
+			while (countdown-- != 0)
+			{
+				_delay_ms(1);
+				if (mrbus_state & MRBUS_RX_PKT_READY)
+					PktHandler();			
+			}
+			DDRC &= ~_BV(PC3);
+
+			// Wait for ack
+			while(!(PINC & _BV(PC3)));
+			
+			PCMSK1 = _BV(PCINT11);
+			PCIFR |= _BV(PCIF1);
+			PCICR |= _BV(PCIE1);
+			TCNT2 = 0;
+			TIMSK2 |= _BV(TOIE2);
+			
+			secs = 0;
+		}
+
+			
+
+		// If we have a packet to be transmitted, try to send it here
+		while(mrbus_state & MRBUS_TX_PKT_READY)
+		{
+			uint8_t bus_countdown;
+
+			// Even while we're sitting here trying to transmit, keep handling
+			// any packets we're receiving so that we keep up with the current state of the
+			// bus.  Obviously things that request a response cannot go, since the transmit
+			// buffer is full.
+			if (mrbus_state & MRBUS_RX_PKT_READY)
+				PktHandler();
+
+
+			if (0 == mrbusPacketTransmit())
+			{
+				mrbus_state &= ~(MRBUS_TX_PKT_READY);
+				break;
+			}
+
+#ifndef MRBEE
+			// If we're here, we failed to start transmission due to somebody else transmitting
+			// Given that our transmit buffer is full, priority one should be getting that data onto
+			// the bus so we can start using our tx buffer again.  So we stay in the while loop, trying
+			// to get bus time.
+
+			// We want to wait 20ms before we try a retransmit
+			// Because MRBus has a minimum packet size of 6 bytes @ 57.6kbps,
+			// need to check roughly every millisecond to see if we have a new packet
+			// so that we don't miss things we're receiving while waiting to transmit
+			bus_countdown = 20;
+			while (bus_countdown-- > 0 && MRBUS_ACTIVITY_RX_COMPLETE != mrbus_activity)
+			{
+				//clrwdt();
+				_delay_ms(1);
+				if (mrbus_state & MRBUS_RX_PKT_READY) 
+					PktHandler();
+			}
+#endif
+		}
+	}
+}
+
+
+
